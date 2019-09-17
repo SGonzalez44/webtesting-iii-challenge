e8beb6cb4a94f2d0b2d644767497ed5bf5de9dc5
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-17 07:13:29 -0700

    got some test to run

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, getByText } from '@testing-library/react';
+
+import Controls from '../controls/Controls';
+
+test('Controls renders without crashing', () => {
+  render(<Controls />);
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react';
+import { react } from '@testing-library/react';
+
+import Display from '../display/Display';
+import { render } from 'react-testing-library';
+
+test('Display is to render without crashing', () => {
+    render(<Display />)
+});
+
+test('Display shows locked if we switch over to locked', () => {
+    const { getByText } = render(<Display locked={true} />);
+    getByText(/locked/i)
+})
+
+test('Display it to render without crashing', () => {
+    const { getByText } = render(<Display locked={false} />);
+    getByText(/locked/i)
+})
