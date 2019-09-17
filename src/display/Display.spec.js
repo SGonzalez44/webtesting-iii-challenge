4ed49e8d59a5e7585d4ddcf7e5aa9782320106e8
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-17 07:19:16 -0700

    added more test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { react } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 
 import Display from '../display/Display';
-import { render } from 'react-testing-library';
 
-test('Display is to render without crashing', () => {
-    render(<Display />)
+test('Display renders without crashing', () => {
+  render(<Display />)
 });
 
-test('Display shows locked if we switch over to locked', () => {
-    const { getByText } = render(<Display locked={true} />);
-    getByText(/locked/i)
+test('Display shows Locked if locked is true', () => {
+  const { getByText } = render(<Display locked={true} />);
+  getByText(/locked/i)
 })
 
-test('Display it to render without crashing', () => {
-    const { getByText } = render(<Display locked={false} />);
-    getByText(/locked/i)
+test('Display shows Unlocked if locked is false', () => {
+  const { getByText } = render(<Display locked={false} />);
+  getByText(/unlocked/i)
+})
+
+test('Display shows Open if closed is false', () => {
+  const { getByText } = render(<Display closed={false} />);
+  getByText(/open/i)
+})
+
+test('Display shows Closed if closed is true', () => {
+  const { getByText } = render(<Display closed={true} />);
+  getByText(/closed/i)
+})
+
+test('Display elements use red-led class when locked or closed', () => {
+  const { getByText } = render(<Display locked={true} closed={true} />);
+  expect(getByText(/closed/i)).toHaveClass('red-led');
+  expect(getByText(/locked/i)).toHaveClass('red-led');
+})
+
+test('Display elements use green-led class when unlocked or open', () => {
+  const { getByText } = render(<Display locked={false} closed={false} />);
+  expect(getByText(/open/i).classList.contains('green-led')).toBe(true);
+  expect(getByText(/unlocked/i).classList.contains('green-led')).toBe(true);
 })
